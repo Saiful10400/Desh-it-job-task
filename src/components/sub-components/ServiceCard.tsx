a4bcc331c2e48580899ebbe9162b2bb51bbfe6bc
saiful10400 <saifulislam10400i5@gmail.com>
2025-10-10 11:22:09 +0600

    cover page is now responsive.

--- src/components/sub-components/ServiceCard.tsx
+++ src/components/sub-components/ServiceCard.tsx
@@ -19,13 +19,13 @@ const ServiceCard = ({ data }: { data: { tittle: string, desc: string } }) => {
 
 
     return (
-        <div className="bg-[#262020] rounded-xl hover:-translate-y-3 duration-300 py-[40px] px-[26px] flex flex-col justify-center items-center text-center">
+        <div className="group bg-[#262020] rounded-xl hover:-translate-y-3 duration-300 py-[40px] px-[26px] flex flex-col justify-center items-center text-center">
             <span style={{ borderRadius: "33% 66% 70% 30% / 49% 62% 38% 51%" }} className=" inline-flex justify-center items-center w-[100px] h-[100px] bg-[#ffbd3a] ">
 
                 {iconFinder()}
 
             </span>
-            <h1 className="mt-[25px] mb-[10px] text-[22px] poppins-bold">{data.tittle}</h1>
+            <h1 className="mt-[25px] group-hover:text-[#ffbd39] mb-[10px] duration-500 transition-all text-[22px] poppins-bold">{data.tittle}</h1>
             <p className="text-[#ccc]">{data.desc}</p>
         </div>
     );
--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -11,9 +11,9 @@ const Home = () => {
         <div>
             <Nav />
             <Cover />
-            <AboutMe />
+            {/* <AboutMe />
             <Service />
-            <Resume />
+            <Resume /> */}
         </div>
     );
 };
